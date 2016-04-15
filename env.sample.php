5b5342d97880e8ea0768b2371a78efb72e6b8b3d
Ben Dechrai <ben@dechrai.com>
2016-04-15 13:20:58 +1000

    Set env vars in php to work with nginx

---
+++ env.sample.php
@@ -0,0 +1,13 @@
+<?php
+
+$_SERVER['SLACK_TOKEN'] = "xoxp-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+$_SERVER['SLACK_DOMAIN'] = "foo.slack.com";
+$_SERVER['SLACK_CHANNELS'] = "general";
+$_SERVER['SLACK_HEADER'] = "Join us on Slack";
+$_SERVER['SLACK_TITLE'] = "Join us on Slack";
+$_SERVER['SLACK_SUB_HEADER'] = "Signup now";
+$_SERVER['SLACK_INVITE_SUCCEED'] = "Congratulations";
+$_SERVER['SLACK_INVITE_FAIL'] = "Error";
+$_SERVER['RECAPTCHA_SITEKEY'] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+$_SERVER['RECAPTCHA_SECRET'] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+
+require '../env.php';
+
 session_start();
 error_reporting(0);
 ini_set('display_errors', 'Off');
